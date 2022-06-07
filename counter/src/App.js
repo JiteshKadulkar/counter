4c09efd50ed798e8f78d46c824ceca9856090776
jitesh <jiteshkadulkar81@gmail.com>
2022-06-07 23:07:48 +0530

    reset master

--- counter/src/App.js
+++ counter/src/App.js
@@ -13,23 +13,12 @@ function App() {
     setCount(count-1)
   }
 
-  const increment3=()=>{
-    setCount(count+3)
-  }
-
-  const decrement3=()=>{
-    setCount(count-3)
-  }
-
   return (
     <div className="App">
       <h1>Counter</h1>
       <h2>Counter value : {count}</h2>
       <button onClick={increment}>Increment by 1</button>
       <button onClick={decrement}>Decrement by 1</button>
-      <h1>Triple increment buttons</h1>
-      <button onClick={increment3}>Increment by 3</button>
-      <button onClick={decrement3}>Decrement by 3</button>
     </div>
   );
 }
